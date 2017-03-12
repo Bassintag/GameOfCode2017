c54bf22deafb32f151fd7a941505768b4b7f441b
fleck_k <kyllian.fleck@epitech.eu>
2017-03-12 03:07:38 +0100

    Splash + Fading

---
+++ Assets/spawntext.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class spawntext : MonoBehaviour {
+
+    public string[] TextSpawn;
+    public Text Textbase;
+
+	void Start () {
+        StartCoroutine(SpawnText());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnText()
+    {
+        for(int i = 0; i < TextSpawn[0].Length; i++)
+        {
+            yield return new WaitForSeconds(0.20f);
+            Textbase.text += TextSpawn[0][i];
+        }
+        yield return new WaitForSeconds(0.55f);
+        for (int i = TextSpawn[0].Length; i != 0; i--)
+        {
+            yield return new WaitForSeconds(0.05f);
+            Textbase.text = Textbase.text.Remove(i - 1, 1);
+        }
+        Textbase.fontSize -= 35;
+        for (int i = 0; i < TextSpawn[1].Length; i++)
+        {
+            yield return new WaitForSeconds(0.15f);
+            Textbase.text += TextSpawn[1][i];
+        }
+        yield return new WaitForSeconds(0.25f);
+        float time = GameObject.Find("FadingSystem").GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(1);
+    }
+}
